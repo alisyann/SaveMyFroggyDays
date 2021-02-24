9b43d2b72be67115aa73d965624d5e436b606d29
npiriou <n.piriou@yahoo.fr>
2021-02-24 10:01:12 +0100

    work with abdou, not done yet

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import CardContainer from "./CardContainer";
 
 import "./Result.css";
 const APIKEY = "5ae2e3f221c38a28845f05b60cb2319b223eb39d80cef2c05a262216";
-const limit = 4;
+const limit = 1;
 const radius = 1000; // in meters
 
 const Result = (props) => {
-  const [arrayActivities, setArrayActivities] = useState(null);
+  const [arrayActivities, setArrayActivities] = useState([]);
   const [offset, setOffset] = useState(0);
   const [loading, setLoading] = useState(false);
-
+  const [city, setCity] = useState("");
+  const [end, setEnd] = useState("");
+  const inter = useRef(null);
+  // Bon le but du jeu c'est de trouver quelle est la condition de fin de votre setInterval une fois qu'on aura ça on va  changer la state fin   si on a ça on est ok partout car on pourra dire si end est égal égal à true  tu clear
+  const clear = () => {
+    console.log("end of activities", inter.current, arrayActivities);
+    window.clearInterval(inter.current);
+  };
   useEffect(() => {
-    async function request(props, offset = 0) {
+    async function request(props, offset) {
       let arrayAct = [];
       const coordsCity = await fetch(
         `http://api.opentripmap.com/0.1/en/places/geoname?apikey=${APIKEY}&name=${props.city}`,
@@ -22,14 +29,20 @@ const Result = (props) => {
       else {
         const { lat, lon } = coordsCity;
 
-        const listActivities = await fetch(
-          `https://api.opentripmap.com/0.1/en/places/radius?apikey=${APIKEY}&radius=${radius}&limit=${limit}&offset=${offset}&lon=${lon}&lat=${lat}&rate=1&format=json`,
-        ).then((res) => res.json());
-
         // can't forEach when using async funcions;
         //for (let i = 0; i < listActivities.length; i++)
         let i = 0;
-        let inter = setInterval(async function () {
+        inter.current = setInterval(async function () {
+          const listActivities = await fetch(
+            `https://api.opentripmap.com/0.1/en/places/radius?apikey=${APIKEY}&radius=${radius}&limit=${limit}&offset=${offset}&lon=${lon}&lat=${lat}&rate=1&format=json`,
+          ).then((res) => res.json());
+          if (!listActivities[i]) {
+            // console.log("chicken", props, listActivities[i].xid, inter);
+            console.log("yolo");
+            clearInterval(inter.current);
+            return;
+          }
+          console.log("nique ta mere ");
           const activitiesData = await fetch(
             `https://api.opentripmap.com/0.1/en/places/xid/${listActivities[i].xid}?apikey=${APIKEY}`,
           ).then((res) => res.json());
@@ -57,16 +70,20 @@ const Result = (props) => {
             (props.checkedShop || !activitiesData.kinds.includes("shops"))
           ) {
             arrayAct.push(activitiesData);
-            console.log(activitiesData);
           }
-          i++;
-          setArrayActivities(arrayAct);
 
+          i++;
+          setOffset(offset + 1);
+          // if(props.city === city)
+          setArrayActivities([...arrayActivities, ...arrayAct]);
+          // console.log(arrayActivities);
           setLoading(false);
           setLoading(true);
-
-          if (i >= listActivities.length) clearInterval(inter);
-        }, 500);
+          setCity(props.city);
+        }, 2000);
+        // console.log("ici c");
+        // if(listActivities[i])
+        //   clear();
       }
     }
     if (props.city === "404") return;
@@ -74,15 +91,19 @@ const Result = (props) => {
       await request(props, offset);
     };
     fetchData();
-  }, [props, offset]);
-
+  }, [arrayActivities, props]);
+  // useEffect(() => {
+  //   clearInterval(inter);
+  // }, [props]);
   useEffect(() => {}, [loading]);
-
-  const handlePage = (next) => {
-    setLoading(false);
-    next ? setOffset(offset + 10) : setOffset(offset - 10);
-    window.scrollTo(0, 0);
-  };
+  // useEffect(() => {
+  //   console.log("city :", city);
+  //   setArrayActivities([]);
+  // }, [city]);
+  // useEffect(() => {
+  //   setPrevCity(props.city);
+  //   console.log("poulet 001:", props.city);
+  // }, []);
 
   return (
     <div className="searchAndResult">
@@ -95,21 +116,6 @@ const Result = (props) => {
           ) : (
             <>
               <CardContainer arrayActivities={arrayActivities} />
-              <div id="divButtonsChangePage">
-                <button
-                  className="buttonChangePage"
-                  onClick={() => handlePage(false)}
-                >
-                  Last
-                </button>
-
-                <button
-                  className="buttonChangePage"
-                  onClick={() => handlePage(true)}
-                >
-                  Next
-                </button>
-              </div>
             </>
           )
         ) : null}
