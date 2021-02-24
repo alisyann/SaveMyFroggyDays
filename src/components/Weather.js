d1e84ae76760935594c68e8331ea622e9841ce7b
Jeromerobichez <jerome.robichez@gmail.com>
2021-02-24 14:56:13 +0100

    clean code comment

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Weather.css";
+import apiKey from './ApiKeyMeteo';
 
 
 import Search from "./Search";
-//api call api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9d986c82c3977e89a2551fa521df3cb1
+
 
 class Weather extends React.Component {
   constructor(props) {
@@ -12,14 +13,10 @@ class Weather extends React.Component {
       response: false,
     };
   }
-  //   minMaxTemp(min, max){
-  //     // if (min && max) {
-  //     //     return (<span className='minMaxTemp'>{props.tempMin}&deg; / {props.tempMax}&deg; </span>)
-  //     // }
-  // };
+ 
   getWeather = async (e) => {
     e.preventDefault();
-    const apiKey = "9d986c82c3977e89a2551fa521df3cb1";
+    
 
     const city = e.target.elements.city.value;
 
